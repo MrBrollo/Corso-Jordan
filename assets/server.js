13684ea0ec0a39c596fef06a170717d94d54b3f5
MrBrollo <147092556+MrBrollo@users.noreply.github.com>
2025-05-26 12:25:18 +0200

    added node.js

---
+++ assets/server.js
@@ -0,0 +1,12 @@
+const express = require("express"); //importo la libreria in pagina
+const path = require("path");
+const app = express(); //la libreria viene inizializzata
+const port = 3000; //porta per il server di sviluppo creato da node.js
+
+// prendo i contenuti statici del sito
+app.use(express.static(path.join(__dirname)));
+
+// chiamata di get di esempio
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
