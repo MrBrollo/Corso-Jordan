7dec1741adc955616d6326ec0b669549876b1860
MrBrollo <147092556+MrBrollo@users.noreply.github.com>
2025-05-29 12:48:30 +0200

    Code has now been commented for better understanding

--- assets/server.js
+++ assets/server.js
@@ -18,17 +18,17 @@ const pool = new Pool({
     port: 5432,
 });
 
-//Middleware
+//Middleware per gestione richieste
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// Static file serving
+// Static file per gli asset JS/CSS
 app.use('/assets', express.static(path.join(__dirname)));
 app.use(express.static(path.join(__dirname, '..')));
 
-//Prendo index.html dalla root
+//route GET per servire index.html
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
@@ -36,6 +36,7 @@ app.get('/', (req, res) => {
 //Gestione POST del form
 
 app.post('/register', async (req, res) => {
+    //destrutturo i dati ricevuti dal client
     const {
         nomeUtente,
         email,
@@ -48,6 +49,7 @@ app.post('/register', async (req, res) => {
     } = req.body;
 
     try {
+        // queery per inserimento dei dati nella tabella
         const query = `
         INSERT INTO "RegistrationForm"
         (nome_utente, email, password, birth_date, phone_number, genere, newsletter, termini)
@@ -64,7 +66,7 @@ app.post('/register', async (req, res) => {
             terms === true
         ];
 
-        await pool.query(query, values)
+        await pool.query(query, values) //esegue la query 
         res.status(200).json({ message: 'Utente registrato con successo!' });
     } catch (error) {
         console.error("Errore durante l'inserimento: ", error);
